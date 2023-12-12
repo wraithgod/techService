f7275c8d7db1226160e220113a4ff203b0385618
wraithgod <wraithgod@ya.ru>
2023-12-12 23:54:56 +0300

    minor submit

---
+++ techService/AppData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace techService
+{
+    public enum TableName { 
+        Clients,
+        EquipmentsToRepair,
+        Requests,
+        Roles,
+        Status,
+        typesOfFault,
+        Users,
+        Workers
+    }
+    internal class AppData
+    {
+        public static techServiceEntities db = new techServiceEntities();
+
+        public static int UsersID;
+    }
+}
--- techService/loginWindow.xaml.cs
+++ techService/loginWindow.xaml.cs
@@ -19,25 +19,53 @@ namespace techService
     /// </summary>
     public partial class loginWindow : Window
     {
-        class User {
+        /*class User {
             string login;
             string password;
             bool isCorrect;
-        }
+        }*/
+
         public loginWindow()
         {
             InitializeComponent();
+           
         }
-
+        
+        
         private void auth_Click(object sender, RoutedEventArgs e)
         {
-            string login = loginBox.Text.ToString();
-            string password = passBox.ToString();
+            try {
+                AppData.
+                techServiceEntities entities = new techServiceEntities();
+                string login = loginBox.Text.ToString();
+                string password = passBox.ToString();
 
-            if () {
-                Window mainWindow = new MainWindow();
-                mainWindow.Show();
-                this.Close();
+                var currentUser = entities.Users.FirstOrDefault((u) => u.login == login && u.password == password);
+
+                if (currentUser == null)
+                {
+                    MessageBox.Show("Данный пользователь не найден");
+
+                }
+                else if (currentUser.login == login && currentUser.password != password)
+                {
+                    MessageBox.Show("Неверный пароль");
+                }
+                else if (currentUser.login == login && currentUser.password == password) {
+                    if (currentUser.roleId == 1)
+                    {
+                        Window mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        this.Close();
+                    }
+                    else {
+                        MessageBox.Show("Users window in progress...");
+                    }
+
+                }
+            }
+            catch (Exception ex){
+                MessageBox.Show($"Ошибка - {ex}");
             }
 
         }        
